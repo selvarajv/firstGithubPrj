d353e612834400da7e2d630e12b48419abc1d076
selvarajv <selvarajv@IMPC1471.htcindia.com>
2014-05-29 14:39:14 +0530

    added a branch firstbranch

--- gitdvc/src/gitdvc/gittest.java
+++ gitdvc/src/gitdvc/gittest.java
@@ -2,7 +2,7 @@ package gitdvc;
 
 public class gittest {
 	 public static void main(String[] args) {
-		    System.out.println("Git is cool");
+		    System.out.println("Git is cools");
 		  }
 }
  
